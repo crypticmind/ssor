ab6a96806c05b1430f315bda5a996c05f14b5481
Carlos Ferreyra <crypticmind@gmail.com>
2017-08-09 17:33:44 -0300

    Working server

--- framework/src/main/scala/crypticmind/ssor/Server.scala
+++ framework/src/main/scala/crypticmind/ssor/Server.scala
@@ -1,151 +1,72 @@
 package crypticmind.ssor
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.StatusCode
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server._
+import akka.stream.ActorMaterializer
 import crypticmind.ssor.model.API
 import crypticmind.ssor.repo.{DepartmentRepo, TeamRepo, UserRepo}
-import spray.json.JsValue
+import sangria.ast
+import sangria.execution.{ErrorWithResolver, Executor, QueryAnalysisError}
+import sangria.marshalling.sprayJson._
+import sangria.parser.QueryParser
+import spray.json._
 
 import scala.concurrent.Future
+import scala.language.postfixOps
+import scala.util.{Failure, Success}
 
-//
-//import akka.actor.ActorSystem
-//import akka.http.scaladsl._
-//import akka.http.scaladsl.server._
-//import akka.http.scaladsl.server.Directives._
-//import akka.stream.ActorMaterializer
-//import sangria.execution.{ErrorWithResolver, Executor, QueryAnalysisError}
-//import sangria.parser._
-//import spray.json._
-//import sangria.marshalling.sprayJson._
-//
-//import scala.util.{Failure, Success}
-//
-//
-//
-//import language.postfixOps
-//
-//import sangria.ast.OperationType
-//import sangria.execution.{ErrorWithResolver, Executor, QueryAnalysisError}
-//import sangria.parser.{QueryParser, SyntaxError}
-//import sangria.marshalling.sprayJson._
-//import spray.json._
-//import akka.http.scaladsl.model.StatusCodes._
-//import akka.stream.actor.{ActorPublisher, ActorSubscriber}
-//import akka.util.Timeout
-//import akka.actor.{ActorSystem, Props}
-//import akka.http.scaladsl.Http
-//import akka.http.scaladsl.server.Directives._
-//import akka.http.scaladsl.server._
-//import akka.stream.ActorMaterializer
-//import akka.stream.scaladsl.{Sink, Source}
-//import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-//import akka.event.Logging
-//import akka.http.scaladsl.marshalling.ToResponseMarshallable
-//import de.heikoseeberger.akkasse._
-//import de.heikoseeberger.akkasse.EventStreamMarshalling._
-//import sangria.execution.deferred.DeferredResolver
-//
-//import scala.concurrent.duration._
-//import scala.util.control.NonFatal
-//import scala.util.{Failure, Success}
-//
 object Server extends App {
-//
-//  implicit val system = ActorSystem("sangria-server")
-//  implicit val materializer = ActorMaterializer()
-//
-//  import system.dispatcher
-//
-//  val route: Route =
-//    (post & path("graphql")) {
-//      entity(as[JsValue]) { requestJson ⇒
-//        graphQLEndpoint(requestJson)
-//      }
-//    } ~
-//      get {
-//        getFromResource("graphiql.html")
-//      }
-//
-//  def graphQLEndpoint(requestJson: JsValue) = {
-//    val JsObject(fields) = requestJson
-//
-//    val JsString(query) = fields("query")
-//
-//    val operation = fields.get("operationName") collect {
-//      case JsString(op) ⇒ op
-//    }
-//
-//    val vars = fields.get("variables") match {
-//      case Some(obj: JsObject) ⇒ obj
-//      case _ ⇒ JsObject.empty
-//    }
-//
-//    QueryParser.parse(query) match {
-//
-//      // query parsed successfully, time to execute it!
-//      case Success(queryAst) ⇒
-//        complete(executeGraphQLQuery(queryAst, operation, vars))
-//
-//      // can't parse GraphQL query, return error
-//      case Failure(error) ⇒
-//        complete(BadRequest, JsObject("error" → JsString(error.getMessage)))
-//    }
-//  }
-//
-//
-//
-//  def executeGraphQLQuery(query: Document, op: Option[String], vars: JsObject) =
-//    Executor.execute(schema, query, new ProductRepo, variables = vars, operationName = op)
-//      .map(OK → _)
-//      .recover {
-//        case error: QueryAnalysisError ⇒ BadRequest → error.resolveError
-//        case error: ErrorWithResolver ⇒ InternalServerError → error.resolveError
-//      }
-//
-//  Http().bindAndHandle(route, "0.0.0.0", 8080)
-//
 
-
-  import sangria.macros._
-  import sangria.execution._
-  import sangria.marshalling.sprayJson._
-  import scala.concurrent.Await
-  import scala.concurrent.duration._
-  import scala.concurrent.ExecutionContext.Implicits.global
-
-//  val query =
-//    graphql"""
-//      query MyUser {
-//        user(id: "1") {
-//          name
-//          team {
-//            name
-//          }
-//        }
-//      }
-//    """
-
-  val query =
-    graphql"""
-      query MyUser {
-        users {
-          id
-          name
-          team {
-            id
-            name
-          }
-        }
-      }
-    """
-
+  implicit val system = ActorSystem("ssor")
+  implicit val materializer = ActorMaterializer()
   val api: API = new API(new UserRepo, new TeamRepo, new DepartmentRepo)
 
-  val result: Future[JsValue] = Executor.execute(api.schema, query, deferredResolver = api.resolver)
+  import system.dispatcher
 
-  val x = Await.result(result, 1.minute)
+  val route: Route =
+    path("graphql") {
+      get {
+        getFromResource("graphiql.html")
+      } ~
+      post {
+        entity(as[JsValue]) { requestJson ⇒
+          graphQLEndpoint(requestJson)
+        }
+      }
+    }
 
-  println(x.prettyPrint)
+  def graphQLEndpoint(requestJson: JsValue): StandardRoute = {
+    val JsObject(fields) = requestJson
 
+    val JsString(query) = fields("query")
 
+    val operation = fields.get("operationName") collect {
+      case JsString(op) ⇒ op
+    }
 
+    val vars = fields.get("variables") match {
+      case Some(obj: JsObject) ⇒ obj
+      case _ ⇒ JsObject.empty
+    }
+
+    QueryParser.parse(query) match {
+      case Success(queryAst) ⇒ complete(executeGraphQLQuery(queryAst, operation, vars))
+      case Failure(error) ⇒ complete(BadRequest, JsObject("error" → JsString(error.getMessage)))
+    }
+  }
+
+  def executeGraphQLQuery(query: ast.Document, op: Option[String], vars: JsObject): Future[(StatusCode with Product with Serializable, JsValue)] =
+    Executor.execute(api.schema, query, deferredResolver = api.resolver, variables = vars, operationName = op)
+      .map(OK → _)
+      .recover {
+        case error: QueryAnalysisError ⇒ BadRequest → error.resolveError
+        case error: ErrorWithResolver ⇒ InternalServerError → error.resolveError
+      }
+
+  Http().bindAndHandle(route, "0.0.0.0", 8080)
 }
