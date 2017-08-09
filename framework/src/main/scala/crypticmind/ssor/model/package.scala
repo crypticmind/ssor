d6dc1d253fac0c6077b5422ca71b798a02240ceb
Carlos Ferreyra <crypticmind@gmail.com>
2017-08-09 16:12:09 -0300

    Refs implicitly mapped as deferred, resolved via implicit fetchers

--- framework/src/main/scala/crypticmind/ssor/model/package.scala
+++ framework/src/main/scala/crypticmind/ssor/model/package.scala
@@ -1,7 +1,7 @@
 package crypticmind.ssor
 
 import crypticmind.ssor.repo.{DepartmentRepo, TeamRepo, UserRepo}
-import sangria.execution.deferred.{Deferred, DeferredResolver, Fetcher, HasId}
+import sangria.execution.deferred.{DeferredResolver, Fetcher, HasId}
 import sangria.schema._
 import sangria.macros.derive._
 
@@ -12,7 +12,10 @@ package object model {
   sealed trait Ref[+T] { def id: String }
 
   object Ref {
-    implicit def hasId[T]: HasId[Ref[T], String] = HasId(_.id)
+    implicit def asId[T](ref: Ref[T]): Id[T] = ref match {
+      case id: Id[T] => id
+      case Persistent(id, _) => Id(id)
+    }
   }
 
   case class Id[+T](id: String) extends Ref[T]
@@ -24,7 +27,7 @@ package object model {
   case class Persistent[+T](id: String, value: T) extends Entity[T] with Ref[T]
 
   object Persistent {
-    implicit def hasId[T]: HasId[Persistent[T], Ref[T]] = HasId(identity)
+    implicit def hasId[T]: HasId[Persistent[T], Id[T]] = HasId(Ref.asId)
   }
 
   case class User(name: String, team: Ref[Team])
@@ -37,19 +40,18 @@ package object model {
 
     implicit val teamType: ObjectType[Unit, Team] = deriveObjectType[Unit, Team](ObjectTypeDescription("A team"))
 
-    case class DeferTeam(team: Ref[Team]) extends Deferred[Persistent[Team]]
-
-    implicit def teamRefAction(rt: Ref[Team]): LeafAction[Unit, Ref[Team]] = DeferredValue(DeferTeam(rt))
-
-    implicit val refTeam: ObjectType[Unit, Ref[Team]] = ref
+    implicit val refTeam: ObjectType[Unit, Ref[Team]] = referenceEntity
 
     implicit val userType: ObjectType[Unit, User] = deriveObjectType[Unit, User](ObjectTypeDescription("A system user"))
 
-    val teams = Fetcher((_: Unit, ids: Seq[Ref[Team]]) => Future.successful(ids.map(ref => teamRepo.getById(ref.id).getOrElse(throw new Exception(s"Unresolved reference to Team with ID ${ref.id}")))))
+    implicit val teams: Fetcher[Unit, Persistent[Team], Persistent[Team], Id[Team]] =
+      Fetcher { (_, refs) =>
+        Future.successful {
+          refs.map(ref => teamRepo.getById(ref.id).getOrElse(throw new Exception(s"Unresolved reference to Team with ID ${ref.id}")))
+        }
+      }
 
     val resolver: DeferredResolver[Unit] = DeferredResolver.fetchers(teams)
-
-    val userTeamResolve: Context[Unit, User] => Action[Unit, Ref[Team]] = c => teams.defer(c.value.team)
 
     val id: Argument[String] = Argument("id", StringType)
 
@@ -79,7 +81,7 @@ package object model {
         Field("id", StringType, resolve = (c: Context[Ctx, Persistent[T]]) => c.value.id) ::
           ot.fields.toList.map(adaptField[Ctx, T, Persistent[T]](_.value)))
 
-    private def ref[Ctx, T](implicit ot: ObjectType[Ctx, T]): ObjectType[Ctx, Ref[T]] =
+    private def referenceEntity[Ctx, T](implicit ot: ObjectType[Ctx, T]): ObjectType[Ctx, Ref[T]] =
       ObjectType(
         s"Reference${ot.name}",
         Field("id", StringType, resolve = (c: Context[Ctx, Ref[T]]) => c.value.id) ::
@@ -87,6 +89,9 @@ package object model {
             case Id(eid) => throw new Exception(s"Unresolved reference to ${ot.name} with ID $eid")
             case Persistent(_, value) => value
           }))
+
+    implicit def refAction[Ctx, T](ref: Ref[T])(implicit fetcher: Fetcher[Ctx, Persistent[T], Persistent[T], Id[T]]): LeafAction[Unit, Ref[T]] =
+      fetcher.defer(ref)
 
     private def adaptField[Ctx, FT, CT](f: CT => FT)(field: Field[Ctx, _]): Field[Ctx, CT] =
       field.copy(resolve = (c: Context[Ctx, CT]) => field.resolve.asInstanceOf[Context[Ctx, FT] => Action[Ctx, _]].apply(Context[Ctx, FT](
